83c019d7ffa40e5a8fe53e1d1f0ab728fd511844
AFiebig <alfredo.fiebig@continuum.cl>
2018-07-09 22:25:47 -0400

    Add OnePay class and Config Test

---
+++ OnePaySDKdotNet/OnePaySDKTest/OnePayConfigTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnePaySDK;
+
+namespace OnePaySDKTest
+{
+    [TestClass]
+    public class OnePayConfigTest
+    {
+        [TestMethod]
+        public void TestOnePayIntegrationType()
+        {
+            Assert.IsTrue(OnePay.IntegrationType.CompareTo(IntegrationType.TEST) == 0);
+            OnePay.IntegrationType = IntegrationType.LIVE;
+            Assert.IsTrue(OnePay.IntegrationType.CompareTo(IntegrationType.LIVE) == 0);
+        }
+    }
+}
