c5a6f3f0e2fcc016f4cd5a0593f28c1310c5f3c8
afiebig <alfredo.fiebig@continuum.cl>
2018-07-10 17:14:20 -0400

    Add cleanup method to test
    had to do this, because the other way to test a static class is using mocks

--- OnePaySDKdotNet/OnePaySDKTest/OnePayConfigTest.cs
+++ OnePaySDKdotNet/OnePaySDKTest/OnePayConfigTest.cs
@@ -7,6 +7,12 @@ namespace OnePaySDKTest
     [TestClass]
     public class OnePayConfigTest
     {
+        [TestCleanup]
+        public void BaseTestCleanup()
+        {
+            OnePay.IntegrationType = IntegrationType.TEST;
+        }
+
         [TestMethod]
         public void TestOnePayIntegrationType()
         {
@@ -28,11 +34,11 @@ namespace OnePaySDKTest
             OnePay.CallbackUrl = "http://localhost:8080/ewallet-endpoints";
             OnePay.SharedSecret = "P4DCPS55QB2QLT56SQH6#W#LV76IAPYX";
 
-            Assert.AreEqual(OnePay.IntegrationType, IntegrationType.TEST);
-            Assert.AreEqual(OnePay.ApiKey, "mUc0GxYGor6X8u-_oB3e-HWJulRG01WoC96-_tUA3Bg");
-            Assert.AreEqual(OnePay.AppKey, "04533c31-fe7e-43ed-bbc4-1c8ab1538afp");
-            Assert.AreEqual(OnePay.CallbackUrl, "http://localhost:8080/ewallet-endpoints");
-            Assert.AreEqual(OnePay.SharedSecret, "P4DCPS55QB2QLT56SQH6#W#LV76IAPYX");
+            Assert.AreEqual(IntegrationType.TEST, OnePay.IntegrationType);
+            Assert.AreEqual("mUc0GxYGor6X8u-_oB3e-HWJulRG01WoC96-_tUA3Bg", OnePay.ApiKey);
+            Assert.AreEqual("04533c31-fe7e-43ed-bbc4-1c8ab1538afp", OnePay.AppKey);
+            Assert.AreEqual("http://localhost:8080/ewallet-endpoints", OnePay.CallbackUrl);
+            Assert.AreEqual("P4DCPS55QB2QLT56SQH6#W#LV76IAPYX", OnePay.SharedSecret);
         }
     }
 }
