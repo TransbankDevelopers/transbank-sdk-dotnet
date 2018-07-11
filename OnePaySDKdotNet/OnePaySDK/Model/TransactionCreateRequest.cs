60625dad03952bb613972c62e61ff8aac5315791
afiebig <alfredo.fiebig@continuum.cl>
2018-07-11 00:27:45 -0400

    Add TransactionCreateRequest object

---
+++ OnePaySDKdotNet/OnePaySDK/Model/TransactionCreateRequest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnePaySDK.Model
+{
+    public sealed class TransactionCreateRequest : BaseRequest
+    {       
+        private List<Item> items;
+        private string callbackUrl;
+        private string channel;
+        private string signature;
+
+        private long ExternalUniqueNumber { get; set; }
+        private long Total { get; set; }
+        private int ItemsQuantity { get; set; }
+        private long IssuedAt { get; set; }
+        public List<Item> Items
+        {
+            get => items;
+            set => items = value ?? throw new ArgumentNullException();
+        }
+        public string CallbackUrl
+        {
+            get => callbackUrl;
+            set => callbackUrl = value ?? throw new ArgumentNullException();
+        }
+        public string Channel
+        {
+            get => channel;
+            set => channel = value ?? throw new ArgumentNullException();
+        }
+        public string Signature
+        {
+            get => signature;
+            set => signature = value ?? throw new ArgumentNullException();
+        }
+
+        public TransactionCreateRequest() { }
+
+        public TransactionCreateRequest(long externalUniqueNumber, long total, int itemsQuantity, long issuedAt, List<Item> items, string callbackUrl, string channel, string signature)
+        {
+            ExternalUniqueNumber = externalUniqueNumber;
+            Total = total;
+            ItemsQuantity = itemsQuantity;
+            IssuedAt = issuedAt;
+            Items = items;
+            CallbackUrl = callbackUrl;
+            Channel = channel;
+            Signature = signature;
+        }
+    }
+}
