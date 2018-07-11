47f80de0403c598cb704773127e4b76c039e1c41
afiebig <alfredo.fiebig@continuum.cl>
2018-07-11 00:25:33 -0400

    Add Item object to send in Transacctions

---
+++ OnePaySDKdotNet/OnePaySDK/Model/Item.cs
@@ -0,0 +1,22 @@
+﻿namespace OnePaySDK.Model
+{
+    public class Item
+    {
+        private string Description { get; set; }
+        private int Quantity { get; set; }
+        private int Amount { get; set; }
+        private string AdditionalData { get; set; }
+        private long Expire { get; set; }
+
+        public Item() { }
+
+        public Item(string description, int quantity, int amount, string additionalData, long expire)
+        {
+            Description = description;
+            Quantity = quantity;
+            Amount = amount;
+            AdditionalData = additionalData;
+            Expire = expire;
+        }
+    }
+}
