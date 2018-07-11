a98b8cc7d34c59a25fc60887becdd0c262c2d137
afiebig <alfredo.fiebig@continuum.cl>
2018-07-11 00:11:41 -0400

    Create an abstract base request

---
+++ OnePaySDKdotNet/OnePaySDK/Model/BaseRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace OnePaySDK.Model
+{
+    public abstract class BaseRequest
+    {
+        private string apiKey { get; set; }
+        private string appKey { get; set; }
+        private readonly bool GenerateOttQrCode = true;
+    }
+}
