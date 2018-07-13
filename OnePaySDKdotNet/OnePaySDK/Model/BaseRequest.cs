b5fc99e23aef4565367f881433a71b154cbae5a4
afiebig <alfredo.fiebig@continuum.cl>
2018-07-13 17:13:11 -0400

    Move BaseRequest from Model to Net

--- OnePaySDKdotNet/OnePaySDK/Model/BaseRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OnePaySDK.Model
-{
-    public abstract class BaseRequest
-    {
-        private string apiKey { get; set; }
-        private string appKey { get; set; }
-    }
-}
---
+++ OnePaySDKdotNet/OnePaySDK/Net/BaseRequest.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Transbank.Model
+{
+    public abstract class BaseRequest
+    {
+        [JsonProperty("apiKey")]
+        public string ApiKey { get; set; }
+        [JsonProperty("appKey")]
+        public string AppKey { get; set; }
+    }
+}
