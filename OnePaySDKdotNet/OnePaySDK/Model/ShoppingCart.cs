28326cc607f1ea9334838c53e45a1964d0572966
afiebig <alfredo.fiebig@continuum.cl>
2018-07-13 17:49:27 -0400

    Add ShippingCart and Transaction params classes

---
+++ OnePaySDKdotNet/OnePaySDK/Model/ShoppingCart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Transbank.Exceptions;
+
+namespace Transbank.Model
+{
+    public sealed class ShoppingCart
+    {
+        private List<Item> Items;
+        public long Total { get; private set; } = 0;
+
+        public ShoppingCart()
+        {
+            Items = new List<Item>();
+        }
+
+        public ReadOnlyCollection<Item> GetItems()
+        {
+            return new ReadOnlyCollection<Item>(Items);
+        }
+
+        public void Add(Item item)
+        {
+            long total = Total + item.Amount;
+            if (total < 0)
+                throw new AmountException(-1,"Total amount can't be less than zero");
+            Total = total;
+            Items.Add(item);
+        }
+
+        public void Remove(Item item)
+        {
+            long total = Total - item.Amount;
+            if (total < 0)
+                throw new AmountException(-1, "Total amount can't be less than zero");
+            Total = total;
+            Items.Remove(item);
+        }
+
+        public int GetItemQuantity()
+        {
+            return Items.Count;
+        }
+    }
+}
---
+++ OnePaySDKdotNet/OnePaySDK/Model/TransactionCreateResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transbank.Model
+{
+    public class TransactionCreateResponse
+    {
+        public string Occ { get; set; }
+        public long Ott { get; set; }
+        public string ExternalUniqueNumber { get; set; }
+        public long IssuedAt { get; set; }
+        public string QrCodeAsBase64 { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Occ={Occ}, Ott={Ott}, " +
+                $"ExternalUniqueNumber={ExternalUniqueNumber}, " +
+                $"QrCodeAsBase64={QrCodeAsBase64}";
+        }
+    }
+
+}
