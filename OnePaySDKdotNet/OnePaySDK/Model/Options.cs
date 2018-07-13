ad2aade36a0a0dd873162acbac64f84ab1a2d140
afiebig <alfredo.fiebig@continuum.cl>
2018-07-13 17:32:55 -0400

    Add Options class to allow users to pass modified params to OnePay Requests

---
+++ OnePaySDKdotNet/OnePaySDK/Model/Options.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transbank.Model
+{
+   public  class Options
+    {
+        public Options()
+        {
+            ApiKey = Options.getDefaults().ApiKey;
+            AppKey = Options.getDefaults().AppKey;
+            SharedSecret = Options.getDefaults().SharedSecret;
+        }
+        public Options(string apiKey, string appKey, string sharedSecret)
+        {
+            ApiKey = apiKey;
+            AppKey = appKey;
+            SharedSecret = sharedSecret;
+        }
+
+        public string ApiKey { get; set; }
+        public string AppKey { get; set; }
+        public string SharedSecret { set; get; }
+
+        public override string ToString()
+        {
+            return $"ApiKey={ApiKey}, AppKey={AppKey}, SharedSecret={SharedSecret}";
+        }
+
+        public static Options getDefaults()
+        {
+            return new Options(OnePay.ApiKey, OnePay.AppKey, OnePay.SharedSecret);
+        }
+    }
+}
