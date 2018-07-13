0b347062e84a9184becab52e3bb2e90db2a73bdc
afiebig <alfredo.fiebig@continuum.cl>
2018-07-13 17:29:52 -0400

    Add utilities to build request and sign them

---
+++ OnePaySDKdotNet/OnePaySDK/Utils/OnepayRequestBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Transbank.Model;
+using Transbank.Net;
+using Transbank.Utils;
+
+namespace Transbank.Utils
+{
+    public class OnePayRequestBuilder
+    {
+        private static volatile OnePayRequestBuilder instance;
+        private static readonly object padlock = new object();
+
+        public SendTransactionRequest build(ShoppingCart cart, Options options)
+        {
+            options = BuildOptions(options);
+            SendTransactionRequest request = new SendTransactionRequest(
+                Guid.NewGuid().ToString(), cart.Total, cart.GetItemQuantity(),
+                    (long)DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond,
+                        cart.GetItems(), OnePay.CallbackUrl, "WEB");
+            PrepareRequest(request, options);
+            return OnePaySignUtil.GetInstance().sign(request, options.SharedSecret);
+        }
+
+        protected Options BuildOptions(Options options)
+        {
+            if (options == null) return Options.getDefaults();
+
+            if (options.ApiKey == null) options.ApiKey = OnePay.ApiKey;
+            if (options.AppKey == null) options.AppKey = OnePay.AppKey;
+            if (options.SharedSecret == null) options.SharedSecret = OnePay.SharedSecret;
+
+            return options;
+        }
+
+        protected void PrepareRequest(BaseRequest request, Options options)
+        {
+            request.ApiKey = options.ApiKey;
+            request.AppKey = options.AppKey;
+        }
+
+        private OnePayRequestBuilder() : base()
+        {
+        }
+
+        public static OnePayRequestBuilder GetInstance()
+        {
+            if (instance == null)
+                lock(padlock)
+                    if (instance == null)
+                        instance = new OnePayRequestBuilder();
+            return instance;
+        }
+        
+    }
+}
