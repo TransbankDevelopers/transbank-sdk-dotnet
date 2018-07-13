71e434b8bceebb86cb8ce7186dc5d5336b239bb0
afiebig <alfredo.fiebig@continuum.cl>
2018-07-13 17:28:41 -0400

    Add TranbankException and sublcasses

---
+++ OnePaySDKdotNet/OnePaySDK/Exceptions/AmountException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Transbank.Exceptions
+{
+    public class AmountException : TransbankException
+    {
+        public AmountException() : base()
+        {
+        }
+
+        public AmountException(int code, string message) 
+            : base(code, message)
+        {
+        }
+
+        public AmountException(int code, string message, Exception innerException) 
+            : base(code, message, innerException)
+        {
+        }
+    }
+}
