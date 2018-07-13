fc7fc097997e8700cf0595bd4069d5ebd0e0e560
afiebig <alfredo.fiebig@continuum.cl>
2018-07-13 16:55:22 -0400

    Rename IntegrationType to OnePayIntegrationType
    Had to change the name because endpoints differ between different products from
    transbank. Also, change implementation from Enum, to class, in order to support
    java weird way to use enums.

---
+++ OnePaySDKdotNet/OnePaySDK/Enums/OnePayIntegrationType.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Transbank.Enums
+{
+    public class OnePayIntegrationType
+    {
+        public static readonly OnePayIntegrationType LIVE = 
+            new OnePayIntegrationType("","LIVE");
+        public static readonly OnePayIntegrationType TEST =
+            new OnePayIntegrationType("https://web2desa.test.transbank.cl", "TEST");
+        public static readonly OnePayIntegrationType MOCK =
+            new OnePayIntegrationType("http://onepay.getsandbox.com", "MOCK");
+
+        private readonly string displayValue;
+        private readonly string key;
+        private static readonly OnePayIntegrationType[] all = new[] { LIVE, TEST, MOCK };
+
+        private OnePayIntegrationType(string displayValue, string key)
+        {
+            this.displayValue = displayValue;
+            this.key = key;
+        }
+
+        public string DisplayValue
+        {
+            get { return displayValue; }
+        }
+
+        public string Key
+        {
+            get { return key; }
+        }
+    }
+}
