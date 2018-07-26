11663199366fedf706f243316798ea1cf41a700f
afiebig <alfredo.fiebig@continuum.cl>
2018-07-26 18:02:42 -0400

    Add Test for Signature

---
+++ Transbank/OnePay/Model/ISignable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transbank.OnePay.Model
+{
+    public interface ISignable
+    {
+        string Signature { get; set; }
+        string GetDataToSign();
+    }
+}
---
+++ Transbank/OnePay/Utils/RequestBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Transbank.OnePay.Model;
+using Transbank.OnePay.Net;
+
+namespace Transbank.OnePay.Utils
+{
+    interface IRequestBuilder
+    {
+        SendTransactionRequest BuildSendTransactionRequest(
+            ShoppingCart cart, Options options);
+        GetTransactionNumberRequest BuildGetTransactionNumberRequest(
+            String occ, String externalUniqueNumber, Options options);
+        NullifyTransactionRequest BuildNullifyTransactionRequest(long amount, 
+            String occ, String externalUniqueNumber,String authorizationCode, Options options);
+    }
+}
