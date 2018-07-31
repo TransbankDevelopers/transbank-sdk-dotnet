619629a92dffb05c7c1205232b1f377b525f0950
afiebig <alfredo.fiebig@continuum.cl>
2018-07-30 22:36:32 -0400

    Add simple test for GetTransactionNumberRequest

---
+++ TransbankTest/TransbankNetTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Transbank.OnePay.Net;
+
+namespace TransbankTest
+{
+    [TestClass]
+    public class TransbankNetTest
+    {
+        [TestMethod]
+        public void GetTransactioNumberRequestTest()
+        {
+            GetTransactionNumberRequest request = new GetTransactionNumberRequest(
+                occ: "0011", externalUniqueNumber: "ABC123", issuedAt: 20
+                );
+
+            Assert.AreEqual("0011", request.Occ);
+            Assert.AreEqual("ABC123", request.ExternalUniqueNumber);
+            Assert.AreEqual(20, request.IssuedAt);
+        }
+    }
+}
