d144a4fc931ed84a5af057a32a84cec1e928ce2e
Rolando Martinez Gallardo <rolandomartinezg@gmail.com>
2018-08-01 21:59:37 -0400

    Remove static httpClient  since it is disposed every time that the client is used.

--- Transbank/Onpay/Net/Channel.cs
+++ Transbank/Onpay/Net/Channel.cs
@@ -6,45 +6,41 @@ using System.Threading.Tasks;
 
 namespace Transbank.Onepay.Net
 {
-    public abstract class Channel
+      public abstract class Channel
     {
         public static async Task<string> RequestAsync(string uri, HttpMethod method, string query)
         {
             return await RequestAsync(uri, method, query, null);
         }
-        public static async Task<string> RequestAsync(string uri, HttpMethod method,
-            string query, string contenType)
+
+        public static async Task<string> RequestAsync(string uri, HttpMethod method, string query, string contenType)
         {
             if (method == null)
                 method = HttpMethod.Get;
             if (contenType == null)
                 contenType = "application/json";
 
-            Client = new HttpClient();
-            var header = new MediaTypeWithQualityHeaderValue(contenType);
-            Client.DefaultRequestHeaders.Accept.Add(header);
-
             HttpRequestMessage message = new HttpRequestMessage(method, new Uri(uri))
             {
                 Content = new StringContent(query, Encoding.UTF8, contenType)
             };
-            try
+
+            using (var client = new HttpClient())
             {
-                HttpResponseMessage response;
-                response = await Client.SendAsync(message);
-                response.EnsureSuccessStatusCode();
-                string jsonResponse = await response.Content.ReadAsStringAsync();
-                return jsonResponse;
-            }
-            finally
-            {
-                Client.Dispose();
-            }
+                    var header = new MediaTypeWithQualityHeaderValue(contenType);                    
+                    client.DefaultRequestHeaders.Accept.Add(header);
+
+                    var response = await client.SendAsync(message);
+                    response.EnsureSuccessStatusCode();
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    return jsonResponse;
+            }            
         }
 
-        internal static HttpClient Client { get; private set; }
 
-        public static string PostString(string url, string RequestMethod, string Query )
+        
+
+        public static string PostString(string url, string RequestMethod, string Query)
         {
             return "";
         }
