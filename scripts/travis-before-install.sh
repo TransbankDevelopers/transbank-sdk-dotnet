6bdcf6a1416a095195cc6d7661ddb527d3321538
afiebig <alfredo.fiebig@continuum.cl>
2018-08-10 12:26:59 -0400

    Add Scripts to simplify the travis.yml file

---
+++ scripts/travis-before-install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.list
+echo "Installing build tools..."
+sudo apt-get -qq update
+sudo apt-get -qq install -y mono-complete unzip wget
+echo "Install MSBuild Sonar scanner"
+wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.0.2.892/sonar-scanner-msbuild-4.0.2.892.zip
+mkdir -p tools/sonar
+unzip -qq sonar.zip -d tools/sonar
+ls -l tools/sonar
+chmod +x tools/sonar/sonar-scanner-3.0.3.778/bin/sonar-scanner
