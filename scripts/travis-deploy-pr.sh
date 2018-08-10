ccb19bf44b5d494e19af2f0e3d7e5d9503e4c6bb
afiebig <alfredo.fiebig@continuum.cl>
2018-08-10 12:02:34 -0400

    Initial Setup for Deploy PR to Gemfury and Tags to NuGet

---
+++ scripts/travis-deploy-pr.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#PR deploys to Gemfury 
+if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+    
+fi
---
+++ scripts/travis-deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#Deploy New Tags to Nuget
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    
+fi
