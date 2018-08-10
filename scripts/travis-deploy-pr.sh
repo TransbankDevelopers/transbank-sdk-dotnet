3eb81b09c878229ea7573b0affcbeb68f941209b
afiebig <alfredo.fiebig@continuum.cl>
2018-08-10 17:33:46 -0400

    Build NuGet package using PR number and CI Build

--- scripts/travis-deploy-pr.sh
+++ scripts/travis-deploy-pr.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd Transbank
-dotnet pack Transbank.csproj -c release --version-suffix ci-`$TRAVIS_PULL_REQUEST` --output nupkgs -v d
+dotnet pack Transbank.csproj -c release --version-suffix ci-`$TRAVIS_PULL_REQUEST`-`$TRAVIS_BUILD_NUMBER` --output nupkgs -v d
 curl -F package=@nupkgs/`ls nupkgs/ | grep nupkg` https://`$GEMFURY_TOKEN`@push.fury.io/h_app104067847/
