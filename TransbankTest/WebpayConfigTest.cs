bd4aefd240713ca547d0d42c28b5406537a41229
afiebig <alfredo.fiebig@continuum.cl>
2019-01-18 15:24:49 -0300

    Add Test for WebPay Notmal TransactioN

---
+++ TransbankTest/WebpayConfigTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Transbank.Webpay;
+
+namespace TransbankTest
+{
+    [TestClass]
+    public class WebpayConfigTest
+    {
+        [TestMethod]
+        public void TestWebpayNormalTransaccionConfig()
+        {
+            var config = Configuration.ForTestingWebpayPlusNormal();
+
+            Assert.AreEqual("597020000540", config.CommerceCode);
+            Assert.AreEqual("INTEGRACION", config.Environment);
+            Assert.AreEqual("", config.Password);
+            Assert.IsTrue(config.PublicCert.Contains(@"\Webpay\IntegrationCerts\tbk.pem"));
+            Assert.IsTrue(config.WebpayCert.Contains(@"\Webpay\IntegrationCerts\WebpayPlusCLP.pfx"));
+        }
+    }
+}
---
+++ TransbankTest/WebpayNormalTransactionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Transbank.Webpay;
+
+namespace TransbankTest
+{
+    [TestClass]
+    public class WebpayNormalTransactionTest
+    {        
+        [TestMethod]
+        public void TestWebpayNormaltransaction()
+        {
+            var transaction = new Webpay(Configuration.ForTestingWebpayPlusNormal()).NormalTransaction;
+
+            Assert.IsNotNull(transaction);
+
+            var initResult = transaction.initTransaction(
+                    1000, new Random().Next(100000, 999999999).ToString(), 
+                        "mi-id-de-sesion", "https://callback/resultado/de/transaccion",
+                            "https://callback/final/post/comprobante/webpay");
+
+            var formAction = initResult.url;
+            Console.WriteLine("URL: " + formAction);
+            var tokenWs = initResult.token;
+            Console.WriteLine("Token: " + tokenWs);
+        }
+    }
+}
