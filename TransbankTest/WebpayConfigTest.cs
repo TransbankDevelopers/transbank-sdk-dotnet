f0d77f3f9a340d6c66df6b10cfe2d52303090671
afiebig <alfredo.fiebig@continuum.cl>
2019-01-30 17:57:47 -0300

    Add test for auto Configurations

--- TransbankTest/WebpayConfigTest.cs
+++ TransbankTest/WebpayConfigTest.cs
@@ -14,8 +14,38 @@ namespace TransbankTest
             Assert.AreEqual("597020000540", config.CommerceCode);
             Assert.AreEqual("INTEGRACION", config.Environment);
             Assert.AreEqual("", config.Password);
-            Assert.IsTrue(config.PublicCert.Contains(@"\Webpay\IntegrationCerts\tbk.pem"));
-            Assert.IsTrue(config.WebpayCert.Contains(@"\Webpay\IntegrationCerts\WebpayPlusCLP.pfx"));
+            Assert.IsTrue(config.TbkPublicCertPath.Contains(@"\TransbankIntegrationPublic.pem"));
+            Assert.IsTrue(config.CommercePfxPath.Contains(@"\WebpayPlusCLP.p12"));
+        }
+        public void ForTestingWebpayOneClickNormal()
+        {
+            var config = Configuration.ForTestingWebpayPlusNormal();
+
+            Assert.AreEqual("597044444405", config.CommerceCode);
+            Assert.AreEqual("INTEGRACION", config.Environment);
+            Assert.AreEqual("12345678", config.Password);
+            Assert.IsTrue(config.TbkPublicCertPath.Contains(@"\TransbankIntegrationPublic.pem"));
+            Assert.IsTrue(config.CommercePfxPath.Contains(@"\WebpayOneClickCLP.p12"));
+        }
+        public void ForTestingWebpayPlusCapture()
+        {
+            var config = Configuration.ForTestingWebpayPlusNormal();
+
+            Assert.AreEqual("597044444404", config.CommerceCode);
+            Assert.AreEqual("INTEGRACION", config.Environment);
+            Assert.AreEqual("12345678", config.Password);
+            Assert.IsTrue(config.TbkPublicCertPath.Contains(@"\TransbankIntegrationPublic.pem"));
+            Assert.IsTrue(config.CommercePfxPath.Contains(@"\WebpayPlusCaptureCLP.p12"));
+        }
+        public void ForTestingWebpayPlusMall()
+        {
+            var config = Configuration.ForTestingWebpayPlusNormal();
+
+            Assert.AreEqual("597044444401", config.CommerceCode);
+            Assert.AreEqual("INTEGRACION", config.Environment);
+            Assert.AreEqual("12345678", config.Password);
+            Assert.IsTrue(config.TbkPublicCertPath.Contains(@"\TransbankIntegrationPublic.pem"));
+            Assert.IsTrue(config.CommercePfxPath.Contains(@"\WebpayPlusMallCLP.p12"));
         }
     }
 }
