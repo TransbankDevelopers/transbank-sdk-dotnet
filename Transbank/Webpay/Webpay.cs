a37e4b921c8eb144f55772372f98ee8e30e8bcfd
afiebig <alfredo.fiebig@continuum.cl>
2019-01-30 12:47:14 -0300

    Remove unnecessary using

--- Transbank/Webpay/Webpay.cs
+++ Transbank/Webpay/Webpay.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 /**
  * @author     Allware Ltda. (http://www.allware.cl)
  * @copyright  2015 Transbank S.A. (http://www.tranbank.cl)
