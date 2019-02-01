fab03e34aba6c1e09e4417ff11b115f85b84c790
afiebig <alfredo.fiebig@continuum.cl>
2019-02-01 15:38:54 -0300

    Remove artifact tag and push it by hand in the before_script

--- scripts/appveyor-before-deploy.ps1
+++ scripts/appveyor-before-deploy.ps1
@@ -11,4 +11,5 @@ else{
     dotnet.exe pack Transbank.csproj -c release --version-suffix ci-$env:APPVEYOR_BUILD_ID --output nupkgs
     Write-Host "Done"
 }
+Push-AppveyorArtifact '.\Transbank\nupkgs\*.nupkg'
 Set-Location ..
