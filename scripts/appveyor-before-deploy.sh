0e121cd0d6e703a4dd1e8cfcd7417c024efd0ea9
afiebig <alfredo.fiebig@continuum.cl>
2019-02-01 14:16:41 -0300

    Debug Deploy Script

--- scripts/appveyor-before-deploy.sh
+++ scripts/appveyor-before-deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+echo "Before Deploy Script"
 cd Transbank
-if ($APPVEYOR_REPO_TAG && (echo $APPVEYOR_REPO_TAG_NAME | egrep -i '^v[0-9]+\.[0-9]+\.[0-9]+'))
+if (echo $APPVEYOR_REPO_TAG_NAME | egrep -i '^v[0-9]+\.[0-9]+\.[0-9]+')
 then
+    echo "Version Tag Found Deploy new Nuget Release"
     VERSION_NUMBER=${APPVEYOR_REPO_TAG_NAME:1}
     dotnet pack Transbank.csproj -c release  -p:Version=$VERSION_NUMBER --output nupkgs -v d
+    echo "Done"
 else
+    echo "No Version Tag Found deploy prerelease to Nuget"
     dotnet pack Transbank.csproj -c release --version-suffix ci-$APPVEYOR_BUILD_ID
+    echo "Done"
 fi
