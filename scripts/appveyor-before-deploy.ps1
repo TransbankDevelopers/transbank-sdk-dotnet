8272cb2d634ab4dd6f5218472e9087e710829545
afiebig <alfredo.fiebig@continuum.cl>
2019-02-05 15:15:42 -0300

    Deploy pre-releases if new build button is preseed
    Done this to avoyd generating a prerelease with every branch merged.

--- scripts/appveyor-before-deploy.ps1
+++ scripts/appveyor-before-deploy.ps1
@@ -6,8 +6,8 @@ if ($env:APPVEYOR_REPO_TAG_NAME -Match "^v[0-9]+\.[0-9]+\.[0-9]+" ){
     dotnet.exe pack Transbank.csproj -c release  -p:Version=$VERSION_NUMBER --output nupkgs
     Write-Host "Done"
 }
-else{
-    Write-Host "No Version Tag Found deploy prerelease to Nuget"
+elseif ($env:APPVEYOR_FORCED_BUILD -Match "true") {
+    Write-Host "New build button presed and no version Tag Found deploy prerelease to Nuget"
     dotnet.exe pack Transbank.csproj -c release --version-suffix ci-$env:APPVEYOR_BUILD_ID --output nupkgs
     Write-Host "Done"
 }
