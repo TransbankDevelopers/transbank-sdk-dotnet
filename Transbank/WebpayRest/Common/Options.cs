13c4c93ea8f50021e3d2020044a0faca1d43d87d
Felipe Fiebig <felipe.fiebig@continuum.cl>
2019-06-19 16:28:18 -0400

    Add Options class

---
+++ Transbank/WebpayRest/Common/Options.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Transbank.Webpay.Common
+{
+    public class Options
+    {
+        private string _commerceCode;
+        private string _apiKey;
+        private WebpayIntegrationType _integrationType;
+
+        public string CommerceCode
+        {
+            get => _commerceCode;
+            set => _commerceCode = value ?? throw new ArgumentNullException(
+                nameof(value), "Commerce code can't be null."
+            );
+        }
+
+        public string ApiKey
+        {
+            get => _apiKey;
+            set => _apiKey = value ?? throw new ArgumentNullException(
+                nameof(value), "Api Key can't be null."
+            );
+        }
+
+        public WebpayIntegrationType IntegrationType
+        {
+            get => _integrationType;
+            set => _integrationType = value ?? throw new ArgumentNullException(
+                nameof(value), "IntegrationType can't be null."
+                );
+        }
+
+        public Options(string commerceCode, string apiKey, WebpayIntegrationType webpayIntegrationType)
+        {
+            CommerceCode = commerceCode;
+            ApiKey = apiKey;
+            IntegrationType = webpayIntegrationType;
+        }
+    }
+}
---
+++ TransbankTest/WebpayRest/WebpayPlus/OptionsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Transbank.Webpay.Common;
+
+namespace TransbankTest.WebpayRest.WebpayPlus
+{
+    [TestClass]
+    public class OptionsTest
+    {
+        [TestMethod]
+        public void TestDefaultConfiguration()
+        {
+            var options = Transbank.Webpay.WebpayPlus.WebpayPlus.DefaultOptions();
+            Assert.AreEqual("597055555532", options.CommerceCode);
+            Assert.AreEqual("579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C", options.ApiKey);
+            Assert.AreEqual(options.IntegrationType, WebpayIntegrationType.Live);
+        }
+
+        [TestMethod]
+        public void TestApiKeyNotNull() => Assert.ThrowsException<ArgumentNullException>(() => new Options(null, "somecommercecode", WebpayIntegrationType.Live));
+
+        [TestMethod]
+        public void TestCommerceCodeNotNull() => Assert.ThrowsException<ArgumentNullException>(() => new Options("someapikey", null, WebpayIntegrationType.Live));
+
+        [TestMethod]
+        public void TestIntegrationTypeNotNull() => Assert.ThrowsException<ArgumentNullException>(() => new Options("someapikey", "somecommercecode", null));
+    }
+}
