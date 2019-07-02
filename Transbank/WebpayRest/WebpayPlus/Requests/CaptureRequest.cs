abaeb909db01abc5cf360f794e9704a5c2ee15f5
Felipe Fiebig <felipe.fiebig@continuum.cl>
2019-07-02 19:05:04 -0400

    add plus transaction capture

---
+++ Transbank/WebpayRest/WebpayPlus/Requests/CaptureRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Transbank.Webpay.Common;
+
+namespace Transbank.Webpay.WebpayPlus.Requests
+{
+    internal class CaptureRequest : BaseRequest
+    {
+        [JsonProperty("buy_order")]
+        internal string BuyOrder { get; set; }
+
+        [JsonProperty("authorization_code")]
+        internal string AuthorizationCode { get; set; }
+
+        [JsonProperty("capture_amount")]
+        internal decimal CaptureAmount { get; set; }
+
+        [JsonProperty("commerce_code", NullValueHandling = NullValueHandling.Ignore)]
+        internal string CommerceCode { get; set; }
+
+        internal CaptureRequest(string token, string buyOrder, string authorizationCode,
+            decimal captureAmount, string commerceCode = null) :
+            base($"/rswebpaytransaction/api/webpay/v1.0/transactions/{token}/capture", HttpMethod.Put)
+        {
+            BuyOrder = buyOrder;
+            AuthorizationCode = authorizationCode;
+            CaptureAmount = captureAmount;
+            CommerceCode = commerceCode;
+        }
+    }
+}
+
+
