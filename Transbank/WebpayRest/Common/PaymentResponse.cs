69071e49c39688de983d07dd3a7da8e4b15a6403
afiebig <alfredo.fiebig@continuum.cl>
2019-07-22 17:04:53 -0400

    Add Payment Request and Response

---
+++ Transbank/WebpayRest/Common/PaymentResponse.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace Transbank.Webpay.Common
+{
+    public class PaymentResponse : PaymentRequest
+    {
+        [JsonProperty("authorization_code")]
+        public int AuthorizationCode { get; private set; }
+
+        [JsonProperty("payment_type_code")]
+        public string PaymentTypeCode { get; private set; }
+
+        [JsonProperty("response_code")]
+        public int ResponseCode { get; private set; }
+
+        [JsonProperty("installments_amount")]
+        public int InstallmentsAmount { get; private set; }
+
+        public PaymentResponse (string comerceCode, string buyOrder, int ammount, int autorizationCode,
+            string paymentTypeCode, int responseCode, int installmentsAmmount) : base(comerceCode, buyOrder, ammount)
+        {
+            AuthorizationCode = autorizationCode;
+            PaymentTypeCode = paymentTypeCode;
+            ResponseCode = responseCode;
+            InstallmentsAmount = installmentsAmmount;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\n" +
+                $"InstallmentsAmount= {InstallmentsAmount}\n" +
+                $"AuthorizationCode= {AuthorizationCode}\n" +
+                $"PaymentTypeCode= {PaymentTypeCode}\n" +
+                $"ResponseCode= {ResponseCode}\n";
+        }
+    }
+}
---
+++ Transbank/WebpayRest/Oneclick/Request/AuthorizeRequest.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Transbank.Webpay.Common;
+
+namespace Transbank.Webpay.Oneclick.Requests
+{
+    internal class AuthorizeRequest : BaseRequest
+    {
+
+        [JsonProperty("username")]
+        public string UserName { get; set; }
+
+        [JsonProperty("tbk_user")]
+        public string TBKUser { get; set; }
+
+        [JsonProperty("buy_order")]
+        public string BuyOrder { get; set; }
+
+        [JsonProperty("details")]
+        public List<PaymentRequest> Details { get; set; }
+
+        public override string ToString()
+        {
+            return $"UserName={UserName}";
+        }
+
+        internal AuthorizeRequest(string userName, string tbkUser, string buyOrder, List<PaymentRequest> details)
+            : base("/rswebpaytransaction/api/oneclick/v1.0/transactions", HttpMethod.Post)
+        {
+            UserName = userName;
+            TBKUser = tbkUser;
+            BuyOrder = buyOrder;
+            Details = details;
+        }
+    }
+}
