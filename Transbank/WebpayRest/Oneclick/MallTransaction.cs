bd196847488538260d24f44bf4bc5bc2b478204c
afiebig <alfredo.fiebig@continuum.cl>
2019-07-31 10:43:27 -0400

    Throw Exception if mall authorization fails

--- Transbank/WebpayRest/Oneclick/MallTransaction.cs
+++ Transbank/WebpayRest/Oneclick/MallTransaction.cs
@@ -1,9 +1,11 @@
+using System;
 using Newtonsoft.Json;
 using Transbank.Common;
-using Transbank.Webpay.Common;
 using Transbank.Exceptions;
+using Transbank.Webpay.Common;
 using Transbank.Webpay.Oneclick.Requests;
 using Transbank.Webpay.Oneclick.Responses;
+using Transbank.Webpay.Oneclick.Exceptions;
 
 namespace Transbank.Webpay.Oneclick
 {
@@ -17,14 +19,19 @@ namespace Transbank.Webpay.Oneclick
         public static AuthorizeResponse Authorize(string userName, string email,
             string responseUrl, Options options)
         {
-            return ExceptionHandler.Perform<AuthorizeResponse, MallTransactionAuthorizeException>(() =>
+            try
             {
                 var startRequest = new StartRequest(userName, email, responseUrl);
-                var response = RequestService.Perform<MallTransactionAuthorizeException>(
-                    startRequest, options);
+                var response = RequestService.Perform(startRequest, options);
 
                 return JsonConvert.DeserializeObject<AuthorizeResponse>(response);
-            });
+            }
+            catch (Exception e)
+            {
+                int code = e.GetType().Equals(typeof(TransbankException)) ?
+                    ((TransbankException)e).Code : -1;
+                throw new MallTransactionAuthorizeException(code, e.Message, e);
+            }
         }
     }
 }
