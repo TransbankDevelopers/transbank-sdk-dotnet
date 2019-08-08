5ce1e5ebf7d4e1ea5c0caa0f2d5ba26410711faf
Gonzalo Castillo Fajardo <goncafa@gmail.com>
2019-08-08 17:36:38 -0400

    WIP: Working on MallDeferredTransaction

--- Transbank/WebpayRest/WebpayPlus/MallDeferredTransaction.cs
+++ Transbank/WebpayRest/WebpayPlus/MallDeferredTransaction.cs
@@ -1,7 +1,36 @@
-﻿namespace Transbank.Webpay.WebpayPlus
+﻿using System;
+using Transbank.Webpay.Common;
+
+namespace Transbank.Webpay.WebpayPlus
 {
     public class MallDeferredTransaction
     {
-        
+        private static string _commerceCode = "597055555544";
+        private static string _apiKey = "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C";
+        private static WebpayIntegrationType _integrationType = WebpayIntegrationType.Test;
+
+        public static string CommerceCode
+        {
+            get => _commerceCode;
+            set => _commerceCode = value ?? throw new ArgumentNullException(
+                                       nameof(value), "Commerce code can't be null."
+                                   );
+        }
+
+        public static string ApiKey
+        {
+            get => _apiKey;
+            set => _apiKey = value ?? throw new ArgumentNullException(
+                                 nameof(value), "Api Key can't be null."
+                             );
+        }
+
+        public static WebpayIntegrationType IntegrationType
+        {
+            get => _integrationType;
+            set => _integrationType = value ?? throw new ArgumentNullException(
+                                          nameof(value), "Integration type can't be null."
+                                      );
+        }
     }
 }
