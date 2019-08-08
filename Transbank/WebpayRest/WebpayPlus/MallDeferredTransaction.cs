6d7e8a2299e4dbca6e9839a7093cc09da5cdf5ae
Gonzalo Castillo Fajardo <goncafa@gmail.com>
2019-08-08 17:34:35 -0400

    Remove logic for class MallDeferredTransaction

--- Transbank/WebpayRest/WebpayPlus/MallDeferredTransaction.cs
+++ Transbank/WebpayRest/WebpayPlus/MallDeferredTransaction.cs
@@ -1,55 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using Transbank.Webpay.Common;
-using Transbank.Webpay.WebpayPlus.Responses;
-
-namespace Transbank.Webpay.WebpayPlus
+﻿namespace Transbank.Webpay.WebpayPlus
 {
     public class MallDeferredTransaction
     {
-        private static string _commerceCode = "597055555544";
-        private static string _apiKey = "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C";
-        private static WebpayIntegrationType _integrationType = WebpayIntegrationType.Test;
-
-        public static string CommerceCode
-        {
-            get => _commerceCode;
-            set => _commerceCode = value ?? throw new ArgumentNullException(
-                                       nameof(value), "Commerce code can't be null."
-                                   );
-        }
-
-        public static string ApiKey
-        {
-            get => _apiKey;
-            set => _apiKey = value ?? throw new ArgumentNullException(
-                                 nameof(value), "Api Key can't be null."
-                             );
-        }
-
-        public static WebpayIntegrationType IntegrationType
-        {
-            get => _integrationType;
-            set => _integrationType = value ?? throw new ArgumentNullException(
-                                          nameof(value), "Integration type can't be null."
-                                      );
-        }
-
-        public static Options DefaultOptions()
-        {
-            return new Options(CommerceCode, ApiKey, IntegrationType);
-        }
-
-        public static MallCreateResponse Create(string buyOrder, string sessionId, string returnUrl,
-            List<TransactionDetail> transactions)
-        {
-            return Create(buyOrder, sessionId, returnUrl, transactions, DefaultOptions());
-        }
-
-        public static MallCreateResponse Create(string buyOrder, string sessionId, string returnUrl,
-            List<TransactionDetail> transactions, Options options)
-        {
-            return MallTransaction.Create(buyOrder, sessionId, returnUrl, transactions, options);
-        }
+        
     }
 }
