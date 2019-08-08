54c6d3d7d78b1269e371ba435f8b53b01c9417af
Gonzalo Castillo Fajardo <goncafa@gmail.com>
2019-08-08 17:48:39 -0400

    WIP: Add static to the class MallDeferredTransaction

--- Transbank/WebpayRest/WebpayPlus/MallDeferredTransaction.cs
+++ Transbank/WebpayRest/WebpayPlus/MallDeferredTransaction.cs
@@ -5,7 +5,7 @@ using Transbank.Webpay.WebpayPlus.Responses;
 
 namespace Transbank.Webpay.WebpayPlus
 {
-    public class MallDeferredTransaction
+    public static class MallDeferredTransaction
     {
         private static string _commerceCode = "597055555544";
         private static string _apiKey = "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C";
