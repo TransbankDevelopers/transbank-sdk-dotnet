3d8f6b9add4a62dd28009cf57a88196dc357b11e
Gonzalo Castillo Fajardo <goncafa@gmail.com>
2019-08-21 15:23:00 -0400

    Add method Capture for WP Plus Mall Diferido

---
+++ Transbank/WebpayRest/WebpayPlus/Requests/MallCaptureRequest.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json;
+using Transbank.Common;
+
+namespace Transbank.WebpayRest.WebpayPlus.Requests
+{
+    public class MallCaptureRequest : BaseRequest
+    {
+        [JsonProperty("commerce_code")]
+        private string CommerceCode { get; set; }
+        
+        [JsonProperty("buy_order")]
+        private string BuyOrder { get; set; }
+        
+        [JsonProperty("authorization_code")]
+        private string AuthorizationCode { get; set; }
+        
+        [JsonProperty("capture_amount")]
+        private decimal CaptureAmount { get; set; }
+        
+        public MallCaptureRequest(string token, string commerceCode, string buyOrder, string authorizationCode,
+            decimal captureAmount)
+            : base($"/rswebpaytransaction/api/webpay/v1.0/transactions/{token}/capture", HttpMethod.Put)
+        {
+            CommerceCode = commerceCode;
+            BuyOrder = buyOrder;
+            AuthorizationCode = authorizationCode;
+            CaptureAmount = captureAmount;
+        }
+
+        public override string ToString()
+        {
+            return
+                $"{nameof(CommerceCode)}: {CommerceCode}, {nameof(BuyOrder)}: {BuyOrder}, {nameof(AuthorizationCode)}: {AuthorizationCode}, {nameof(CaptureAmount)}: {CaptureAmount}";
+        }
+    }
+}
