4f3ac5022fd3dd8dcdc1805315caadda6e2f8431
Alba Cárdenas <alba.cardenas@continuum.cl>
2019-08-21 17:03:36 -0400

    Update mall transaction to use the new classes structure

--- Transbank/WebpayRest/Oneclick/MallTransaction.cs
+++ Transbank/WebpayRest/Oneclick/MallTransaction.cs
@@ -1,42 +1,68 @@
 using System;
 using Newtonsoft.Json;
-using Transbank.Common;
 using System.Collections.Generic;
-using Transbank.Exceptions;
+using Transbank.Common;
 using Transbank.Webpay.Common;
 using Transbank.Webpay.Oneclick.Requests;
 using Transbank.Webpay.Oneclick.Responses;
 using Transbank.Webpay.Oneclick.Exceptions;
 
-
 namespace Transbank.Webpay.Oneclick
 {
     public static class MallTransaction
     {
+        private static string _commerceCode = "597055555541";
+        private static string _apiKey = "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C";
+        private static string[] _storeCodes = { "597055555542", "597055555543" };
+
+        private static WebpayIntegrationType _integrationType = WebpayIntegrationType.Test;
+
+        public static string CommerceCode
+        {
+            get => _commerceCode;
+            set => _commerceCode = value ?? throw new ArgumentNullException(
+                                       nameof(value), "Commerce code can't be null."
+                                   );
+        }
+
+        public static string ApiKey
+        {
+            get => _apiKey;
+            set => _apiKey = value ?? throw new ArgumentNullException(
+                                 nameof(value), "Api Key can't be null."
+                             );
+        }
+
+        public static WebpayIntegrationType IntegrationType
+        {
+            get => _integrationType;
+            set => _integrationType = value ?? throw new ArgumentNullException(
+                                          nameof(value), "Integration type can't be null."
+                                      );
+        }
+
+        public static Options DefaultOptions()
+        {
+            return new Options(CommerceCode, ApiKey, IntegrationType);
+        }
+        
         public static AuthorizeMallResponse Authorize(string userName, string tbkUser,
             string buyOrder, List<PaymentRequest> details)
         {
-            return Authorize(userName, tbkUser, buyOrder, details, Oneclick.DefaultOptions());
+            return Authorize(userName, tbkUser, buyOrder, details, DefaultOptions());
         }
 
-        public static AuthorizeMallResponse Authorize(string userName,
-            string tbkUser, string buyOrder, List<PaymentRequest> details,
-            Options options)
+        public static AuthorizeMallResponse Authorize(string userName, string tbkUser, string buyOrder,
+            List<PaymentRequest> details, Options options)
         {
-            try
+            return ExceptionHandler.Perform<AuthorizeMallResponse, MallTransactionAuthorizeException>(() =>
             {
                 var authorizeRequest = new AuthorizeMallRequest(userName, tbkUser, buyOrder,
                     details);
-                var response = RequestService.Perform(authorizeRequest, options);
+                var response = RequestService.Perform<MallTransactionAuthorizeException>(authorizeRequest, options);
 
                 return JsonConvert.DeserializeObject<AuthorizeMallResponse>(response);
-            }
-            catch (Exception e)
-            {
-                int code = e.GetType().Equals(typeof(TransbankException)) ?
-                    ((TransbankException)e).Code : -1;
-                throw new MallTransactionAuthorizeException(code, e.Message, e);
-            }
+            });
         }
     }
 }
