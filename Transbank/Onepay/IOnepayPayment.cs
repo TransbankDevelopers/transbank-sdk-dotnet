e8e3137ae54d5ae5fbfd54e5852a6c9127463129
afiebig <alfredo.fiebig@continuum.cl>
2019-10-17 10:18:49 -0300

    Add Interface for users of weboscket class to implement

---
+++ Transbank/Onepay/IOnepayPayment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+
+
+namespace Transbank.Onepay
+{
+    public interface IOnepayPayment
+    {
+        void Connected();
+        void NewMessage(string payload);
+        void Disconnected();
+    }
+
+}
