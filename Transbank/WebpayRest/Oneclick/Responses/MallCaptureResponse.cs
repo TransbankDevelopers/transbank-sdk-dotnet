a5d5006ff962895ad32c9e6b526f8ea5ad0406ea
afiebig <alfredo.fiebig@continuum.cl>
2020-12-16 16:49:09 -0300

    Add Deferred Capture

---
+++ Transbank/WebpayRest/Oneclick/Responses/MallCaptureResponse.cs
@@ -0,0 +1,35 @@
+using Newtonsoft.Json;
+
+namespace Transbank.WebpayRest.Oneclick.Responses
+{
+    public class MallCaptureResponse
+    {
+        [JsonProperty("authorization_code")]
+        public string AuthorizationCode { get; set; }
+        
+        [JsonProperty("authorization_date")]
+        public string AuthorizationDate { get; set; }
+        
+        [JsonProperty("captured_amount")]
+        public double CapturedAmount { get; set; }
+        
+        [JsonProperty("response_code")]
+        public byte ResponseCode { get; set; }
+
+        public MallCaptureResponse(string authorizationCode, string authorizationDate, double capturedAmount, byte responseCode)
+        {
+            AuthorizationCode = authorizationCode;
+            AuthorizationDate = authorizationDate;
+            CapturedAmount = capturedAmount;
+            ResponseCode = responseCode;
+        }
+
+        public override string ToString()
+        {
+            return $"\"AuthorizationCode\": \"{AuthorizationCode}\"\n" +
+                   $"\"AuthorizationDate\": \"{AuthorizationDate}\"\n" +
+                   $"\"CapturedAmount\": \"{CapturedAmount}\"\n" +
+                   $"\"ResponseCode\":\"{ResponseCode}\"";
+        }
+    }
+}
