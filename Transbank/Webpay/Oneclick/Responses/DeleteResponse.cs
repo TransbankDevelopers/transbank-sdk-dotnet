eac6fd5cd4490e1ffdf24c55c5b714de153f2dee
Felipe Fiebig <felipe.fiebig@continuum.cl>
2021-05-26 10:45:43 -0400

    Remove Obsolete Classes

--- Transbank/Webpay/Oneclick/Responses/DeleteResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace Transbank.Webpay.Oneclick.Responses
-{
-    [Obsolete("Use MallDeleteresponse instead", false)]
-    public class DeleteResponse : MallDeleteResponse { }
-}
