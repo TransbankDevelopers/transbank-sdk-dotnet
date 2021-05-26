15dda9243ddbf3c705405a50c9d75654c9a9e53b
Felipe Fiebig <felipe.fiebig@continuum.cl>
2021-05-26 10:50:57 -0400

    Restore OnepayPayment Interface usings

--- Transbank/Onepay/IOnepayPayment.cs
+++ Transbank/Onepay/IOnepayPayment.cs
@@ -1,4 +1,11 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+
 namespace Transbank.Onepay
 {
     public interface IOnepayPayment
