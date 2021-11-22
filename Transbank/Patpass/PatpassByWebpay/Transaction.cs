36fe0fb042740aa7f58380356a366c637a4417b7
DarkFrost <m.astudillo.toledo@gmail.com>
2021-11-22 15:01:48 -0300

    Add object support to Patpass

--- Transbank/Patpass/PatpassByWebpay/Transaction.cs
+++ Transbank/Patpass/PatpassByWebpay/Transaction.cs
@@ -8,94 +8,79 @@ using Transbank.Patpass.PatpassByWebpay.Responses;
 
 namespace Transbank.Patpass.PatpassByWebpay
 {
-    public static class Transaction
+    public class Transaction
     {
-        private static string _commerceCode = "597055555550";
-        private static string _apiKey = "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C";
-        private static PatpassByWebpayIntegrationType _integrationType = PatpassByWebpayIntegrationType.Test;
+        public Options Options { get; private set; }
 
-        public static string CommerceCode
+        public Transaction() : this(
+            new Options(
+                IntegrationCommerceCodes.WEBPAY_PLUS,
+                IntegrationApiKeys.WEBPAY,
+                PatpassByWebpayIntegrationType.Test
+            )
+        )
+        { }
+
+        public Transaction(Options options)
         {
-            get => _commerceCode;
-            set => _commerceCode = value ?? throw new ArgumentNullException(
-                nameof(value), "Commerce code can't be null."
-            );
+            Options = options;
         }
 
-        public static string ApiKey
-        {
-            get => _apiKey;
-            set => _apiKey = value ?? throw new ArgumentNullException(
-                nameof(value), "Api Key can't be null."
-            );
-        }
-
-        public static PatpassByWebpayIntegrationType IntegrationType
-        {
-            get => _integrationType;
-            set => _integrationType = value ?? throw new ArgumentNullException(
-                nameof(value), "Integration type can't be null."
-            );
-        }
-
-        public static Options DefaultOptions()
-        {
-            return new Options(CommerceCode, ApiKey, IntegrationType);
-        }
-
-        public static CreateResponse Create(string buyOrder, string sessionId, decimal amount, string returnUrl, string serviceId, string cardHolderId,
+        public CreateResponse Create(string buyOrder, string sessionId, decimal amount, string returnUrl, string serviceId, string cardHolderId,
                 string cardHolderName, string cardHolderLastName1, string cardHolderLastName2, string cardHolderMail, string cellphoneNumber,
                 string expirationDate, string commerceMail, bool ufFlag)
-        {
-            return Create(buyOrder, sessionId, amount, returnUrl, serviceId, cardHolderId,
-                cardHolderName, cardHolderLastName1, cardHolderLastName2, cardHolderMail, cellphoneNumber,
-                expirationDate, commerceMail, ufFlag, DefaultOptions());
-        }
-
-        public static CreateResponse Create(string buyOrder, string sessionId, decimal amount, string returnUrl, string serviceId, string cardHolderId,
-                string cardHolderName, string cardHolderLastName1, string cardHolderLastName2, string cardHolderMail, string cellphoneNumber,
-                string expirationDate, string commerceMail, bool ufFlag, Options options)
         {
             var createRequest = new CreateRequest(buyOrder, sessionId, amount, returnUrl, serviceId, cardHolderId,
                 cardHolderName, cardHolderLastName1, cardHolderLastName2, cardHolderMail, cellphoneNumber,
                 expirationDate, commerceMail, ufFlag);
-            var response = RequestService.Perform<TransactionCreateException>(createRequest, options);
+            var response = RequestService.Perform<TransactionCreateException>(createRequest, Options);
 
             return JsonConvert.DeserializeObject<CreateResponse>(response);
         }
 
-        public static CommitResponse Commit(string token)
-        {
-            return Commit(token, DefaultOptions());
-        }
-
-        public static CommitResponse Commit(string token, Options options)
+        public CommitResponse Commit(string token)
         {
             return ExceptionHandler.Perform<CommitResponse, TransactionCommitException>(() =>
             {
                 var commitRequest = new CommitRequest(token);
                 var response = RequestService.Perform<TransactionCommitException>(
-                    commitRequest, options);
+                    commitRequest, Options);
 
                 return JsonConvert.DeserializeObject<CommitResponse>(response);
             });
         }
 
-        public static StatusResponse Status(string token)
-        {
-            return Status(token, DefaultOptions());
-        }
-
-        public static StatusResponse Status(string token, Options options)
+        public StatusResponse Status(string token)
         {
             return ExceptionHandler.Perform<StatusResponse, TransactionStatusException>(() =>
             {
                 var statusRequest = new StatusRequest(token);
                 var response = RequestService.Perform<TransactionStatusException>(
-                    statusRequest, options);
+                    statusRequest, Options);
 
                 return JsonConvert.DeserializeObject<StatusResponse>(response);
             });
+        }
+
+        /*
+        |--------------------------------------------------------------------------
+        | Environment Configuration
+        |--------------------------------------------------------------------------
+        */
+
+        public void ConfigureForIntegration(String commerceCode, String apiKey)
+        {
+            Options = new Options(commerceCode, apiKey, PatpassByWebpayIntegrationType.Test);
+        }
+
+        public void ConfigureForProduction(String commerceCode, String apiKey)
+        {
+            Options = new Options(commerceCode, apiKey, PatpassByWebpayIntegrationType.Live);
+        }
+
+        public void ConfigureForTesting()
+        {
+            ConfigureForIntegration(IntegrationCommerceCodes.PATPASS_BY_WEBPAY, IntegrationApiKeys.WEBPAY);
         }
     }
 }
--- Transbank/Patpass/PatpassComercio/Inscription.cs
+++ Transbank/Patpass/PatpassComercio/Inscription.cs
@@ -9,47 +9,32 @@ using Transbank.Patpass.PatpassComercio.Responses;
 
 namespace Transbank.Patpass.PatpassComercio
 {
-    public static class Inscription
+    public class Inscription
     {
-        private static string _commerceCode = "28299257";
-        private static string _apiKey = "cxxXQgGD9vrVe4M41FIt";
-        private static PatpassComercioIntegrationType _integrationType = PatpassComercioIntegrationType.Test;
-
         // The authentication headers for this product are different, these have
         // to be used. You can put them in the Perform method of the RequestService
-        private static string _apiKeyHeaderName = "Authorization";
-        private static string _commerceCodeHeaderName = "commerceCode";
+        private string _apiKeyHeaderName = "Authorization";
+        private string _commerceCodeHeaderName = "commerceCode";
 
-        private static RequestServiceHeaders _headers = new RequestServiceHeaders(_apiKeyHeaderName, _commerceCodeHeaderName);
+        private RequestServiceHeaders _headers;
+        public Options Options { get; private set; }
 
-        public static string CommerceCode
+        public Inscription() : this(
+            new Options(
+                IntegrationCommerceCodes.WEBPAY_PLUS,
+                IntegrationApiKeys.WEBPAY,
+                PatpassComercioIntegrationType.Test
+            )
+        )
+        { }
+
+        public Inscription(Options options)
         {
-            get => _commerceCode;
-            set => _commerceCode = value ?? throw new ArgumentNullException(
-                                       nameof(value), "Commerce Code can't be null."
-            );
+            Options = options;
+            _headers = new RequestServiceHeaders(_apiKeyHeaderName, _commerceCodeHeaderName);
         }
 
-        public static string ApiKey
-        {
-            get => _apiKey;
-            set => _apiKey = value ?? throw new ArgumentNullException(
-                                 nameof(value), "Api Key can't be null.");
-        }
-
-        public static PatpassComercioIntegrationType IntegrationType
-        {
-            get => _integrationType;
-            set => _integrationType = value ?? throw new ArgumentNullException(
-                                          nameof(value), "Integration type can't be null");
-        }
-
-        public static Options DefaultOptions()
-        {
-            return new Options(CommerceCode, ApiKey, IntegrationType);
-        }
-
-        public static StartResponse Start(
+        public StartResponse Start(
             string url,
             string name,
             string fLastname,
@@ -67,65 +52,55 @@ namespace Transbank.Patpass.PatpassComercio
             string city
         )
         {
-            return Start(
-                url, name, fLastname, sLastname, rut, serviceId, finalUrl,
-                maxAmount, phoneNumber, mobileNumber, patpassName, personEmail,
-                commerceEmail, address, city, DefaultOptions()
-            );
-        }
-
-        public static StartResponse Start(
-            string url,
-            string name,
-            string fLastname,
-            string sLastname,
-            string rut,
-            string serviceId,
-            string finalUrl,
-            decimal maxAmount,
-            string phoneNumber,
-            string mobileNumber,
-            string patpassName,
-            string personEmail,
-            string commerceEmail,
-            string address,
-            string city,
-            Options options
-        )
-        {
             // set culture to es-CL, since webpay only works with clp we are forcing to anyone to use clp currency standard.
-            CultureInfo myCiIntl = new CultureInfo("es-CL", false);  
-            string  mAmount = maxAmount <= 0 ? "" : maxAmount.ToString(myCiIntl);   
+            CultureInfo myCiIntl = new CultureInfo("es-CL", false);
+            string mAmount = maxAmount <= 0 ? "" : maxAmount.ToString(myCiIntl);
             return ExceptionHandler.Perform<StartResponse, InscriptionStartException>(() =>
             {
                 var request = new StartRequest(
                     url, name, fLastname, sLastname, rut, serviceId, finalUrl,
-                    options.CommerceCode, mAmount, phoneNumber, mobileNumber,
+                    Options.CommerceCode, mAmount, phoneNumber, mobileNumber,
                     patpassName, personEmail, commerceEmail, address, city
                 );
-                var response = RequestService.Perform<InscriptionStartException>(request, options, _headers);
+                var response = RequestService.Perform<InscriptionStartException>(request, Options, _headers);
 
                 return JsonConvert.DeserializeObject<StartResponse>(response);
             });
-            
+
         }
 
-        public static StatusResponse Status(string token)
-        {
-            return Status(token, DefaultOptions());
-        }
-
-        public static StatusResponse Status(string token, Options options)
+        public StatusResponse Status(string token)
         {
             return ExceptionHandler.Perform<StatusResponse, InscriptionStatusException>(() =>
             {
                 var statusRequest = new StatusRequest(token);
                 var response = RequestService.Perform<InscriptionStatusException>(
-                    statusRequest, options, _headers);
+                    statusRequest, Options, _headers);
 
                 return JsonConvert.DeserializeObject<StatusResponse>(response);
             });
         }
+
+        /*
+        |--------------------------------------------------------------------------
+        | Environment Configuration
+        |--------------------------------------------------------------------------
+        */
+
+        public void ConfigureForIntegration(String commerceCode, String apiKey)
+        {
+            Options = new Options(commerceCode, apiKey, PatpassComercioIntegrationType.Test);
+        }
+
+        public void ConfigureForProduction(String commerceCode, String apiKey)
+        {
+            Options = new Options(commerceCode, apiKey, PatpassComercioIntegrationType.Live);
+        }
+
+        public void ConfigureForTesting()
+        {
+            ConfigureForIntegration(IntegrationCommerceCodes.PATPASS_COMERCIO, IntegrationApiKeys.PATPASS_COMERCIO);
+        }
     }
-    
+
 }
