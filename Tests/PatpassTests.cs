01659f38972da6ab3078208bfa3207851db040b1
Matias Vasquez <m.vasquezac@gmail.com>
2024-05-22 15:58:49 -0400

    test: update use of options on patpass

--- Tests/PatpassTests.cs
+++ Tests/PatpassTests.cs
@@ -11,9 +11,9 @@ namespace Tests
         public void CorrectOptions()
         {
             Inscription inscription = Inscription.buildForIntegration(IntegrationCommerceCodes.PATPASS_COMERCIO, IntegrationApiKeys.PATPASS_COMERCIO);
-            Assert.Equal(IntegrationCommerceCodes.PATPASS_COMERCIO, inscription.options.CommerceCode);
-            Assert.Equal(IntegrationApiKeys.PATPASS_COMERCIO, inscription.options.ApiKey);
-            Assert.Equal("https://pagoautomaticocontarjetasint.transbank.cl", inscription.options.IntegrationType.ApiBase);
+            Assert.Equal(IntegrationCommerceCodes.PATPASS_COMERCIO, inscription.Options.CommerceCode);
+            Assert.Equal(IntegrationApiKeys.PATPASS_COMERCIO, inscription.Options.ApiKey);
+            Assert.Equal("https://pagoautomaticocontarjetasint.transbank.cl", inscription.Options.IntegrationType.ApiBase);
         }
 
     }
