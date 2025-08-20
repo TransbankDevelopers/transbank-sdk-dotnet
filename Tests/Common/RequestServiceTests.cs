635a4b3a76b0df86c96d425d54b5754e476c45c8
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2025-08-20 16:16:41 -0400

    test: update request service test

--- Tests/Common/RequestServiceTests.cs
+++ Tests/Common/RequestServiceTests.cs
@@ -2,7 +2,9 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Moq;
+using Newtonsoft.Json;
 using Transbank.Common;
+using Transbank.Exceptions;
 using Xunit;
 
 namespace Transbank.Tests.Common
@@ -10,23 +12,88 @@ namespace Transbank.Tests.Common
     public class RequestServiceTests
     {
         [Fact]
-        public async Task RequestService_UsesInjectedIHttpClient()
+        public void RequestService_UsesInjectedIHttpClient()
         {
             var mockHttpClient = new Mock<IHttpClient>();
             var expectedResponse = new HttpResponseMessage(HttpStatusCode.OK)
             {
-                Content = new StringContent("{\"ok\":true}")
+                Content = new StringContent("{\"field\":\"value\"}")
             };
             mockHttpClient.Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>()))
-                .ReturnsAsync(expectedResponse);
+                .ReturnsAsync(expectedResponse)
+                .Verifiable();
 
             var requestService = new RequestService(mockHttpClient.Object);
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://fake.url");
+            var request = new DummyRequest();
+            var options = new DummyOptions();
 
-            var response = await mockHttpClient.Object.SendAsync(request);
+            var result = requestService.Perform<DummyResponse, DummyException>(request, options);
 
-            Assert.NotNull(response);
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(result);
+            Assert.Equal("value", result.Field);
+            mockHttpClient.Verify(x => x.SendAsync(It.IsAny<HttpRequestMessage>()), Times.Once);
+        }
+
+        [Fact]
+        public void Perform_ShouldReturnDeserializedResponse_WhenHttpIsSuccessful()
+        {
+            var mockHttpClient = new Mock<IHttpClient>();
+            var responseJson = "{\"field\":\"value\"}";
+            var httpResponse = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(responseJson)
+            };
+            mockHttpClient.Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>()))
+                .ReturnsAsync(httpResponse);
+
+            var service = new RequestService(mockHttpClient.Object);
+            var request = new DummyRequest();
+            var options = new DummyOptions();
+
+            var result = service.Perform<DummyResponse, DummyException>(request, options);
+
+            Assert.NotNull(result);
+            Assert.Equal("value", result.Field);
+        }
+
+        [Fact]
+        public void Perform_ShouldThrowException_WhenHttpFails()
+        {
+            var mockHttpClient = new Mock<IHttpClient>();
+            var httpResponse = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+            {
+                Content = new StringContent("{ \"error_message\": \"fail\" }")
+            };
+            mockHttpClient.Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>()))
+                .ReturnsAsync(httpResponse);
+
+            var service = new RequestService(mockHttpClient.Object);
+            var request = new DummyRequest();
+            var options = new DummyOptions();
+
+            Assert.Throws<DummyException>(() => service.Perform<DummyResponse, DummyException>(request, options));
+        }
+
+        public class DummyIntegrationType : IIntegrationType
+        {
+            public string ApiBase => "https://dummy.api";
+            public string Key => "dummy";
+        }
+        public class DummyRequest : BaseRequest
+        {
+            public DummyRequest() : base("/dummy", HttpMethod.Post) { }
+        }
+        public class DummyResponse : BaseResponse
+        {
+            public string Field { get; set; }
+        }
+        public class DummyException : TransbankException
+        {
+            public DummyException(int code, string message) : base(code, message) { }
+        }
+        public class DummyOptions : Options
+        {
+            public DummyOptions() : base("code", "key", new DummyIntegrationType(), 10, null) { }
         }
     }
 }
